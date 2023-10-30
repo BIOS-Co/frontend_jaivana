7ddc7e4664dd429a46ba73a3b89fab44a93df805
Sebastian Mendez <mendezsebas8@gmail.com>
2023-10-29 23:27:59 -0500

    "Terminamos pestaña de lobby"

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,27 +3,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import './Navbar.css';
 
 export default function Navigationbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" id='nav_bar' className="bg-body-tertiary" style={{position:'absolute','width':'100%',opacity:0.6}}>
       <Container>
-        <Navbar.Brand href="#/Lobby">Bios</Navbar.Brand>
+        <Navbar.Brand style={{cursor:'default'}} className='titleNav' href="#/Lobby">JAIVANÁ - SUMATEC</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="https://empresa.sumatec.co/">Nosotros</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+          <Nav className="me-auto" style={{display:'flex',alignItems:'center',justifyContent:'center','width':'300px'}}>
+            <NavDropdown style={{width:'300px !important'}} className='font_' title="Paginas" id="basic-nav-dropdown">
+              <NavDropdown.Item className='font_' href="https://www.sumatec.co/">Sumatec</NavDropdown.Item>
+              <NavDropdown.Item className='font_' href="https://www.jaivanaweb.co/">Jaivaná</NavDropdown.Item>
+              <NavDropdown.Item className='font_' href="https://www.bios.co/">Bios co.</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
--- src/Router/Lobby/Lobby.jsx
+++ src/Router/Lobby/Lobby.jsx
@@ -6,16 +6,26 @@ import Carousel from 'react-bootstrap/Carousel';
 import Imagen_1 from '../../img/Fondo_1.jpg';
 import Imagen_2 from '../../img/Fondo_2.jpg';
 import Imagen_3 from '../../img/Fondo_3.jpg';
-import Fondo from '../../img/Background_2.jpg';
+import Fondo from '../../img/Background_1.jpg';
+import '../../index.css';
 
 export default function Lobby() {
 
 
   return (
     <div className='body'>
-        
-        <div className='carouselBody' style={{backgroundImage: `url(${Fondo})`,backgroundSize:'cover'}}>
-            
+        <Navigationbar ></Navigationbar>
+        <div className='carouselBody font' style={{backgroundImage: `url(${Fondo})`,backgroundSize:'cover'}}>
+            <p className='font title ' id='dropdown-basic'>¿Que deseas realizar?</p>
+            <p className='font description'>Selecciona una de las dos opciones para hacer inferencia, en el cual puedes encontrar nuestro sistema de recomendación de productos o fijación de precios</p>
+            <div className='cardsContainer'>
+                <div className='card' style={{backgroundImage: `url(${Imagen_3})`,backgroundSize:'cover',opacity:0.4}}>
+                    <button className='textCard' style={{position:'relative',bottom:'12px'}}>Recomendación de productos</button>
+                </div>
+                <div className='card' style={{backgroundImage: `url(${Imagen_2})`,backgroundSize:'cover',opacity:0.4}}>
+                    <p className='textCard'>Fijado de precios</p>
+                </div>
+            </div>
         </div>
         
     </div>
